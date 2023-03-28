b58ecc6c33e231edf454f5bee7be75b6ec6c559c
Zibusiso Mafaiti <zimafaiti@gmail.com>
2023-03-28 13:54:34 -0600

    ability to display project name instead of the id

--- models/studentsmongoose.js
+++ models/studentsmongoose.js
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
 
-
 const studentsSchema = mongoose.Schema({
   Password: String,
   name: String,
   email: String,
   Grade: Number,
-  Projects: [{type:mongoose.Schema.Types.ObjectId,
-  ref:"projects"}],
+  Projects: [{ type: mongoose.Schema.Types.ObjectId, ref: "projects" }],
 });
 export const students = mongoose.model("students", studentsSchema);
-
 
 //const studentData = await students.find();
 
@@ -20,27 +17,21 @@ export const addAStudent = async (newStudent) => {
   return 0;
 };
 
-export const changeStudent= async (studentEmail, update) => {
-  let test = await students.findOneAndReplace({email:studentEmail},update)
+export const changeStudent = async (studentEmail, update) => {
+  let test = await students.findOneAndReplace({ email: studentEmail }, update);
   console.log(test);
   console.log("found", studentEmail);
-}
+};
 
-export const addProjectStudent = async(studentEmail, projectId) => {
-   let selectedStudent = await students.findOne({email:studentEmail})
+export const addProjectStudent = async (studentEmail, projectId) => {
+  let selectedStudent = await students.findOne({ email: studentEmail });
   selectedStudent.Projects.push(projectId);
-  await selectedStudent.save()
-// await students.replaceOne({selectedStudent},selectedStudent)
-    
-  
-}
-
-
+  await selectedStudent.save();
+  // await students.replaceOne({selectedStudent},selectedStudent)
+};
 
 export const getAllStudents = async () => {
- let data =  await students.find({});
+  let data = await students.find({}).populate("Projects", "project_title");
   console.log(data);
-  return(data);
-
-}
-
+  return data;
+};
